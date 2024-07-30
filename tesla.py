3bf07dc5a5cf268a602296c0c3abd3b6208ae1e0
Nonoo <nonoo@nonoo.hu>
2024-07-30 21:08:41 +0200

    Rename global variables to avoid conflicts

--- tesla.py
+++ tesla.py
@@ -6,24 +6,25 @@ import teslapy
 import os
 import time
 
-mutex = multiprocessing.Lock()
+tesla_mutex = multiprocessing.Lock()
 
 tesla_stream_update_timeout_sec = 30
 tesla_stream_process_handle = None
 
-vehicle_last_seen_ts = None
-vehicle_charge_percent = None
-vehicle_lat = None
-vehicle_lng = None
-vehicle_speed_kmh = None
-vehicle_heading = None
-vehicle_altitude_m = None
-vehicle_range_km = None
-vehicle_shift_state = None
+tesla_vehicle_last_seen_ts = None
+tesla_vehicle_charge_percent = None
+tesla_vehicle_lat = None
+tesla_vehicle_lng = None
+tesla_vehicle_speed_kmh = None
+tesla_vehicle_heading = None
+tesla_vehicle_altitude_m = None
+tesla_vehicle_range_km = None
+tesla_vehicle_shift_state = None
 
 def tesla_get_data():
-    with mutex:
-	    return vehicle_last_seen_ts, vehicle_charge_percent, vehicle_lat, vehicle_lng, vehicle_speed_kmh, vehicle_heading, vehicle_altitude_m, vehicle_range_km, vehicle_shift_state
+    with tesla_mutex:
+	    return tesla_vehicle_last_seen_ts, tesla_vehicle_charge_percent, tesla_vehicle_lat, tesla_vehicle_lng, tesla_vehicle_speed_kmh, \
+            tesla_vehicle_heading, tesla_vehicle_altitude_m, tesla_vehicle_range_km, tesla_vehicle_shift_state
 
 def tesla_init(email):
     tesla = teslapy.Tesla(email)
@@ -100,48 +101,48 @@ def tesla_stream_process_stop():
         tesla_stream_process_handle = None
 
 def tesla_stream_process_data(data):
-    with mutex:
+    with tesla_mutex:
         log("Got Tesla update:")
         if 'timestamp' in data:
-            global vehicle_last_seen_ts
-            vehicle_last_seen_ts = int(data['timestamp'] / 1000) # Convert ms to s
-            log(f"  Timestamp: {vehicle_last_seen_ts}")
+            global tesla_vehicle_last_seen_ts
+            tesla_vehicle_last_seen_ts = int(data['timestamp'] / 1000) # Convert ms to s
+            log(f"  Timestamp: {tesla_vehicle_last_seen_ts}")
         if 'soc' in data:
-            global vehicle_charge_percent
-            vehicle_charge_percent = data['soc']
-            log(f"  Charge percent: {vehicle_charge_percent}%")
+            global tesla_vehicle_charge_percent
+            tesla_vehicle_charge_percent = data['soc']
+            log(f"  Charge percent: {tesla_vehicle_charge_percent}%")
         if 'est_lat' in data:
-            global vehicle_lat
-            vehicle_lat = data['est_lat']
-            log(f"  Latitude: {vehicle_lat}")
+            global tesla_vehicle_lat
+            tesla_vehicle_lat = data['est_lat']
+            log(f"  Latitude: {tesla_vehicle_lat}")
         if 'est_lng' in data:
-            global vehicle_lng
-            vehicle_lng = data['est_lng']
-            log(f"  Longitude: {vehicle_lng}")
+            global tesla_vehicle_lng
+            tesla_vehicle_lng = data['est_lng']
+            log(f"  Longitude: {tesla_vehicle_lng}")
         if 'speed' in data:
-            global vehicle_speed_kmh
-            vehicle_speed_kmh = data['speed']
-            if not vehicle_speed_kmh:
-                vehicle_speed_kmh = 0
+            global tesla_vehicle_speed_kmh
+            tesla_vehicle_speed_kmh = data['speed']
+            if not tesla_vehicle_speed_kmh:
+                tesla_vehicle_speed_kmh = 0
             else:
-                vehicle_speed_kmh = int(vehicle_speed_kmh * 1.60934) # Convert mph to kmh
-            log(f"  Speed: {vehicle_speed_kmh}km/h")
+                tesla_vehicle_speed_kmh = int(tesla_vehicle_speed_kmh * 1.60934) # Convert mph to kmh
+            log(f"  Speed: {tesla_vehicle_speed_kmh}km/h")
         if 'est_heading' in data:
-            global vehicle_heading
-            vehicle_heading = data['est_heading']
-            log(f"  Heading: {vehicle_heading}")
+            global tesla_vehicle_heading
+            tesla_vehicle_heading = data['est_heading']
+            log(f"  Heading: {tesla_vehicle_heading}")
         if 'elevation' in data:
-            global vehicle_altitude_m
-            vehicle_altitude_m = data['elevation']
-            log(f"  Altitude: {vehicle_altitude_m}m")
+            global tesla_vehicle_altitude_m
+            tesla_vehicle_altitude_m = data['elevation']
+            log(f"  Altitude: {tesla_vehicle_altitude_m}m")
         if 'range' in data:
-            global vehicle_range_km
-            vehicle_range_km = int(data['range'] * 1.60934) # Convert miles to km
-            log(f"  Range: {vehicle_range_km}km")
+            global tesla_vehicle_range_km
+            tesla_vehicle_range_km = int(data['range'] * 1.60934) # Convert miles to km
+            log(f"  Range: {tesla_vehicle_range_km}km")
         if 'shift_state' in data:
-            global vehicle_shift_state
-            vehicle_shift_state = data['shift_state']
-            log(f"  Shift state: {vehicle_shift_state}")
+            global tesla_vehicle_shift_state
+            tesla_vehicle_shift_state = data['shift_state']
+            log(f"  Shift state: {tesla_vehicle_shift_state}")
 
 def tesla_update_force(tesla, vehicle_nr, wake_up):
     result = True
@@ -152,7 +153,7 @@ def tesla_update_force(tesla, vehicle_nr, wake_up):
             log("  Waking up vehicle...")
             vehicle.sync_wake_up()
 
-        with mutex:
+        with tesla_mutex:
             log("Forced Tesla update results:")
             vehicle_state = vehicle['vehicle_state']
             log(f"  Vehicle name: {vehicle_state['vehicle_name']}")
@@ -163,42 +164,42 @@ def tesla_update_force(tesla, vehicle_nr, wake_up):
                 log("  Mobile access: disabled")
 
             drive_state = vehicle['drive_state']
-            global vehicle_last_seen_ts
-            vehicle_last_seen_ts = drive_state['gps_as_of']
-            log(f"  Timestamp: {vehicle_last_seen_ts}")
+            global tesla_vehicle_last_seen_ts
+            tesla_vehicle_last_seen_ts = drive_state['gps_as_of']
+            log(f"  Timestamp: {tesla_vehicle_last_seen_ts}")
 
-            global vehicle_charge_percent
+            global tesla_vehicle_charge_percent
             charge_state = vehicle['charge_state']
-            vehicle_charge_percent = charge_state['battery_level']
-            log(f"  Charge percent: {vehicle_charge_percent}%")
+            tesla_vehicle_charge_percent = charge_state['battery_level']
+            log(f"  Charge percent: {tesla_vehicle_charge_percent}%")
 
-            global vehicle_lat
-            vehicle_lat = drive_state['latitude']
-            log(f"  Latitude: {vehicle_lat}")
+            global tesla_vehicle_lat
+            tesla_vehicle_lat = drive_state['latitude']
+            log(f"  Latitude: {tesla_vehicle_lat}")
 
-            global vehicle_lng
-            vehicle_lng = drive_state['longitude']
-            log(f"  Longitude: {vehicle_lng}")
+            global tesla_vehicle_lng
+            tesla_vehicle_lng = drive_state['longitude']
+            log(f"  Longitude: {tesla_vehicle_lng}")
 
-            global vehicle_speed_kmh
-            vehicle_speed_kmh = drive_state['speed']
-            if not vehicle_speed_kmh:
-                vehicle_speed_kmh = 0
+            global tesla_vehicle_speed_kmh
+            tesla_vehicle_speed_kmh = drive_state['speed']
+            if not tesla_vehicle_speed_kmh:
+                tesla_vehicle_speed_kmh = 0
             else:
-                vehicle_speed_kmh = int(vehicle_speed_kmh * 1.60934) # Convert mph to kmh
-            log(f"  Speed: {vehicle_speed_kmh}km/h")
+                tesla_vehicle_speed_kmh = int(tesla_vehicle_speed_kmh * 1.60934) # Convert mph to kmh
+            log(f"  Speed: {tesla_vehicle_speed_kmh}km/h")
 
-            global vehicle_heading
-            vehicle_heading = drive_state['heading']
-            log(f"  Heading: {vehicle_heading}")
+            global tesla_vehicle_heading
+            tesla_vehicle_heading = drive_state['heading']
+            log(f"  Heading: {tesla_vehicle_heading}")
 
-            global vehicle_range_km
-            vehicle_range_km = int(charge_state['battery_range'] * 1.60934) # Convert miles to km
-            log(f"  Range: {vehicle_range_km}km")
+            global tesla_vehicle_range_km
+            tesla_vehicle_range_km = int(charge_state['battery_range'] * 1.60934) # Convert miles to km
+            log(f"  Range: {tesla_vehicle_range_km}km")
 
-            global vehicle_shift_state
-            vehicle_shift_state = drive_state['shift_state']
-            log(f"  Shift state: {vehicle_shift_state}")
+            global tesla_vehicle_shift_state
+            tesla_vehicle_shift_state = drive_state['shift_state']
+            log(f"  Shift state: {tesla_vehicle_shift_state}")
     except Exception as e:
         log(f"Forced Tesla update failed: {e}")
         result = False
